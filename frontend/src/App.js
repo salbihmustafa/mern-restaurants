6cfddababc30c18832fc84d0afed164e0228fc3b
Salbih Mustafa <salbih.mustafa@certegy.com>
2022-10-16 16:23:01 -0500

    added container to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import {useState} from "react";
 import {Route, Routes, Navigate} from "react-router-dom";
+import {Container, Row} from "react-bootstrap";
 
 import Navbar from "./components/Navigation/Navbar";
 import AddReview from "./pages/AddReview";
@@ -22,16 +23,20 @@ function App() {
     return (
         <div className="App">
             <Navbar logout={logout} user={user}/>
-            <Routes>
-                <Route path='*' element={<Navigate to='/restaurants' replace/>}/>
-                <Route exact path={'/restaurants'} element={<RestaurantList/>}/>
-                <Route exact path='/restaurants/:id/review'
-                       element={<AddReview user={user}/>}/>
-                <Route exact path='/restaurants/:id'
-                       element={<Restaurant user={user}/>}/>
-                <Route exact path='/login'
-                       element={<Login login={login}/>}/>
-            </Routes>
+            <Container>
+                <Row>
+                    <Routes>
+                        <Route path='*' element={<Navigate to='/restaurants' replace/>}/>
+                        <Route exact path={'/restaurants'} element={<RestaurantList/>}/>
+                        <Route exact path='/restaurants/:id/review'
+                               element={<AddReview user={user}/>}/>
+                        <Route exact path='/restaurants/:id'
+                               element={<Restaurant user={user}/>}/>
+                        <Route exact path='/login'
+                               element={<Login login={login}/>}/>
+                    </Routes>
+                </Row>
+            </Container>
         </div>
     );
 }
