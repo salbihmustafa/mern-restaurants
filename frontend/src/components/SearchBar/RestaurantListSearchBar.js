685939b67dc3373767299d9a4cb4f5cbcdbd9e58
Salbih Mustafa <salbih.mustafa@certegy.com>
2022-10-26 12:39:21 -0500

    update dropdown values

--- frontend/src/components/SearchBar/RestaurantListSearchBar.js
+++ frontend/src/components/SearchBar/RestaurantListSearchBar.js
@@ -9,14 +9,14 @@ const RestaurantListSearchBar = () => {
         <>
             <Form className="d-flex">
                 <DropdownButton className="searchbar-dropdown-toggle" title="" onSelect={handleDropdownSelect}>
-                    <Dropdown.Item eventKey='Action'>Action</Dropdown.Item>
-                    <Dropdown.Item eventKey='Another Action'>Another action</Dropdown.Item>
-                    <Dropdown.Item eventKey='Something else'>Something else</Dropdown.Item>
+                    <Dropdown.Item eventKey='name'>Name</Dropdown.Item>
+                    <Dropdown.Item eventKey='zip'>Zip</Dropdown.Item>
+                    <Dropdown.Item eventKey='cuisine'>Cuisine</Dropdown.Item>
                 </DropdownButton>
                 <Form.Control
                     type="search"
                     placeholder="Search"
-                    className="search-bar"
+                    className="me-2 search-bar"
                     aria-label="Search"
                 />
                 <Button>Search</Button>
